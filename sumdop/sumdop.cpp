8626f4a0939ebdd2ad2951f60354f8bfe680b865
Егор <@DESKTOP-1FF7P13>
2023-05-17 18:53:37 +0300

    Добавьте файлы проекта.

---
+++ sumdop/sumdop.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int number, sum = 0;
+    std::cout << "Введите целое число: ";
+    std::cin >> number;
+
+    // Складываем цифры числа
+    while (number != 0) {
+        sum += number % 10;
+        number /= 10;
+    }
+
+    std::cout << "Сумма цифр числа: " << sum << std::endl;
+
+    return 0;
+}
